53c7b15c016357adfb0644d7a168198619ac190f
Phanthat Muenprap <phanthat54@gmail.com>
2022-01-28 14:48:48 +0700

    init 9x9 board

---
+++ GoGame.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int board[] = {
+    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 1, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7,
+    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
+};
+
+int board_size = 9;
+int margin = 2;
+int board_range = board_size + margin;
+
+int square, stone;
+char pieces[] = {'.','#','o',' ',' ','b','w',' ','+'};
+
+void print_board(){
+    // loop rows
+    for (int row=0; row<board_range; row++){
+        //loop columns
+        for (int col=0; col<board_range; col++){
+            square = row * board_range + col;
+            stone = board[square];
+
+            if (col == 0 && row > 0 && row < board_range - 1){
+                cout <<" " <<10-row;
+            }
+            //print board
+            cout << pieces[stone] <<" ";
+        }
+        //print new line
+        cout <<"\n";
+    }
+    cout <<"    a b c d e f g h i";
+}
+
+int main(){
+    print_board();
+
+    return 0;
+}
